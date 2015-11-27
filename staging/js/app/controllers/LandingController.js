43d0d5859d15a8735525503fd1c973427530ffb8
Christopher Spence <spencech@gmail.com>
2015-11-27 12:32:29 -0500

    removing staging vc

--- staging/js/app/controllers/LandingController.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('bingo')
-  .controller('LandingController', function ($scope) {
-  	var trace = angular.trace;
-    trace("controller loaded");
-  });
--- staging/php/scripts/bingo.php
+++
@@ -1,6 +0,0 @@
-<?php
-	
-	$result = new stdClass();
-	$result->hello = "world";
-	print json_encode($result);
-?>
