df89d9f92c83e62b689745137fd946fe63e63fda
Christopher Spence <spencech@gmail.com>
2016-01-21 13:21:03 -0500

    some changes for the postmessage stuff

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -2,6 +2,7 @@ var bingo = (function(self){
 
 	angular.module('bingo', []).config(function($sceProvider){
 		$sceProvider.enabled(false);
+		setTimeout(function(){window.parent.postMessage({quantity:5},"http://bingo-development.myshopify.com"); angular.trace("posted")},5000);
 	});
 
 	angular.trace = function (){
